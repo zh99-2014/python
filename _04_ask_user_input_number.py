01f524d98b2069e3829b073d98540702ebdc88a6
zhao chenyao <zhaochenyao1997@gmail.com>
2016-11-24 17:31:59 +0800

    Update _04_ask_user_input_number.py

--- _04_ask_user_input_number.py
+++ _04_ask_user_input_number.py
@@ -1,12 +1,11 @@
-'''
-salary = input("please enter your salary")
-bonus = input("please enter your bonus")
-paycheckAmount = salary + bonus
-print(paycheckAmount)
+#salary = input("please enter your salary")
+#bonus = input("please enter your bonus")
+#paycheckAmount = salary + bonus
+#print(paycheckAmount)
 
-this code has a bug: salary and bonus are both string, + only to combine salarybonus
-e.g. salary = 500; bonus = 75; paycheckAmount = 50075
-'''
+#this code has a bug: salary and bonus are both string, + only to combine salarybonus
+#e.g. salary = 500; bonus = 75; paycheckAmount = 50075
+
 
 #this is OK
 salary = input("please enter your salary ")
